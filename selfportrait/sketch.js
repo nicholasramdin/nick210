b86a6518604c9a19679e6b6ce8e94f7e9e28b67a
nicholasramdin <nicholasramdin@gmail.com>
2017-09-05 21:06:40 -0400

    Initial commit

---
+++ selfportrait/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(640 , 460);
+}
+function draw() {
+    background("white");
+    rect(100, 100, 200, 30);
+    ellipse(200, 200, 50);
+    line(500, 200, 400, 400);
+    
+}
